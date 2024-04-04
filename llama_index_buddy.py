dbe630b85d46cb1e81d69ad0491ff5b6c14cafcd
Raghavan <raghavan.mit@gmail.com>
2024-04-03 22:42:55 -0700

    building the same application using llama index starter kit

---
+++ llama_index_buddy.py
@@ -0,0 +1,32 @@
+import sys
+import os.path
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+
+directory = sys.argv[1]
+query = sys.argv[2]
+
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+# check if index already exists
+PERSIST_DIR = "./" + directory + "_index"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader(directory).load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+# Either way we can now query the index
+query_engine = index.as_query_engine()
+response = query_engine.query(query)
+print(response)
